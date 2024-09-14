473aeb7c54614ebcf787d7d9fee2492297d065aa
Andre Diniz <andre_fcd@yahoo.com.br>
2024-09-14 22:55:06 +0100

    Criando um docker-compose para utilizar o mysql, fazer as configuracoes do db e criando as funcoes para inserir e atualizar um produto

--- 4-Banco_de_Dados/main.go
+++ 4-Banco_de_Dados/main.go
@@ -1,5 +1,67 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"github.com/google/uuid"
+	//esse underline significa blank identifier ou seja, ele ira ignorar o erro por nao estar utilizando e compilar
+	_ "github.com/go-sql-driver/mysql"
+)
 
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	product := NewProduct("Notebook", 1999.90)
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+
+	product.Price = 900.00
+	err = updateProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("INSERT INTO products(id, name, price) VALUES(?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("UPDATE products SET name = ?, price = ? WHERE id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
