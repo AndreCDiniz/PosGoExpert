a07fb169a8400bb0a0cc9a82fb8a0ea0d3c8096f
Andre Diniz <andre_fcd@yahoo.com.br>
2024-09-15 09:45:39 +0100

    Finalizado crud usando o Go sem utilizar ORM

--- 4-Banco_de_Dados/main.go
+++ 4-Banco_de_Dados/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/google/uuid"
 	//esse underline significa blank identifier ou seja, ele ira ignorar o erro por nao estar utilizando e compilar
 	_ "github.com/go-sql-driver/mysql"
@@ -21,23 +22,51 @@ func NewProduct(name string, price float64) *Product {
 }
 
 func main() {
+
+	//Criando conexao com DB
+	//para acessar a base de dados pelo docker compose usamos no bash o mysql -root -p goexpert(nome do banco que criamos)
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
+	//Inserindo valores no DB
 	product := NewProduct("Notebook", 1999.90)
 	err = insertProduct(db, product)
 	if err != nil {
 		panic(err)
 	}
 
+	//Fazendo update no DB
 	product.Price = 900.00
 	err = updateProduct(db, product)
 	if err != nil {
 		panic(err)
 	}
+
+	//Fazendo select em um product
+	//p, err := selectProduct(db, product.ID)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Printf("Product: %v, possui o preço de %.2f", p.Name, p.Price)
+
+	//Fazendo select em varios produtos
+	products, err := selectAllProducts(db)
+	if err != nil {
+		panic(err)
+	}
+	for _, p := range products {
+		fmt.Printf("Product: %v, possui o preço de %.2f\n", p.Name, p.Price)
+	}
+
+	//deletando produto
+	err = deleteProduct(db, product.ID)
+	if err != nil {
+		panic(err)
+	}
+
 }
 
 func insertProduct(db *sql.DB, product *Product) error {
@@ -60,6 +89,51 @@ func updateProduct(db *sql.DB, product *Product) error {
 	}
 	defer stmt.Close()
 	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func selectProduct(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("SELECT id, name, price FROM products WHERE id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var p Product
+	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
+}
+
+func selectAllProducts(db *sql.DB) ([]Product, error) {
+	rows, err := db.Query("SELECT id, name, price FROM products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var products []Product
+	for rows.Next() {
+		var p Product
+		err = rows.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+	return products, nil
+}
+
+func deleteProduct(db *sql.DB, id string) error {
+	stmt, err := db.Prepare("DELETE FROM products WHERE id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(id)
 	if err != nil {
 		return err
 	}
