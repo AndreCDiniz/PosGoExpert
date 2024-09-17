3f94176738cbedd02b6e431c74dad0886fc48ac2
Andre Diniz <andre_fcd@yahoo.com.br>
2024-09-17 23:59:45 +0100

    Criado todos as funcoes. Quando é utilizado o gorm.Model é criado nas tabelas o created_at, updated_at e o deleted_at e também as deleçoes são feitas como "soft delete" ou seja, elas não são deletadas da base de dados para poder manter a consistência das informações.

--- 4-Banco_de_Dados/Com_ORM/main.go
+++ 4-Banco_de_Dados/Com_ORM/main.go
@@ -9,10 +9,11 @@ type Product struct {
 	ID    int `gorm:"primary_key"`
 	Name  string
 	Price float64
+	gorm.Model
 }
 
 func main() {
-	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
@@ -26,10 +27,56 @@ func main() {
 	//})
 
 	//create batchn
-	products := []Product{
-		{Name: "PC Gamer", Price: 1000.00},
-		{Name: "PC Normal", Price: 500.00},
-		{Name: "PC Ruim", Price: 200.00},
-	}
-	db.Create(&products)
+	//products := []Product{
+	//	{Name: "PC Gamer", Price: 1000.00},
+	//	{Name: "PC Normal", Price: 500.00},
+	//	{Name: "PC Ruim", Price: 200.00},
+	//}
+	//db.Create(&products)
+
+	//selecionar o primeiro que tenha PC Gamer
+	//var product Product
+	//db.First(&product, "name = ?", "PC Gamer")
+	//fmt.Println(product)
+
+	//select all
+	//var products []Product
+	//db.Find(&products)
+	//for _, product := range products {
+	//	fmt.Println(product)
+	//}
+
+	//O limit usamos para limitar a quantidade de coisas que buscamos
+	//enquanto o offset é utilizado para paginação.
+	//var products []Product
+	//db.Limit(2).Offset(2).Find(&products)
+	//for _, product := range products {
+	//	fmt.Println(product)
+	//}
+
+	//usando where
+	//var products []Product
+	//db.Where("price > ?", 300).Find(&products)
+	//for _, product := range products {
+	//	fmt.Println(product)
+	//}
+
+	//	//usando like
+	//	//var products []Product
+	//	//db.Where("name LIKE ?", "%Gamer").Find(&products)
+	//	//for _, product := range products {
+	//	//	fmt.Println(product)
+	//}
+
+	//Fazendo alteração do dado
+	//var p Product
+	//db.First(&p, 1)
+	//p.Name = "Mouse"
+	//db.Save(&p)
+
+	//Fazendo deleção de um dado
+	//var p2 Product
+	//db.First(&p2, 1)
+	//db.Delete(&p2)
+
 }
