fa8586582321d09388c8d098a4cb7acae17f0463
Andre Diniz <andre_fcd@yahoo.com.br>
2024-12-22 18:42:52 +0000

    feat: add new package about tests and create some unit tests and benchmarks

---
+++ 6-Testing/SimpleTest/tax.go
@@ -0,0 +1,23 @@
+package SimpleTest
+
+import "time"
+
+// To create a covarage test you can run
+// go test -cover ./... -coverprofile=coverage.out
+// and to see it in the html file
+// go tool cover -html coverage
+func CalculateTax(amount float64) float64 {
+	if amount >= 1000 {
+		return 10.0
+	}
+	return 5.0
+}
+
+// Create to use in benchmark tests
+func CalculateTaxTime(amount float64) float64 {
+	time.Sleep(time.Millisecond)
+	if amount >= 1000 {
+		return 10.0
+	}
+	return 5.0
+}
