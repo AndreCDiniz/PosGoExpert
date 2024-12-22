71511d78a1472ac9ca24bd25a56d9e5dd25b4905
Andre Diniz <andre_fcd@yahoo.com.br>
2024-12-22 19:10:19 +0000

    feat: add new fuzz tests, and in this commit we have some problems in the test that we will fix in the next one.

--- 6-Testing/SimpleTest/tax.go
+++ 6-Testing/SimpleTest/tax.go
@@ -7,8 +7,14 @@ import "time"
 // and to see it in the html file
 // go tool cover -html coverage
 func CalculateTax(amount float64) float64 {
+	if amount < 0 {
+		return 0
+	}
 	if amount >= 1000 {
 		return 10.0
+	}
+	if amount >= 20000 {
+		return 20.0
 	}
 	return 5.0
 }
