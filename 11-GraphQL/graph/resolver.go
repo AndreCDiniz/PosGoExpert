9c4f070d376f7c0def8f2eed3064fbf5c309526b
Andre Diniz <andre_fcd@yahoo.com.br>
2025-02-12 23:21:40 +0000

    feat: create categories in graphql and the create method

---
+++ 11-GraphQL/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/AndreCDiniz/PosGoExpert/11-GraphQL/internal/database"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CategoryDB *database.Category
+}
